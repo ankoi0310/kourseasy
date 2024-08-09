78b869cb20e9c371a27064bad6fc32bab2fa30fc
Koi <huynhvahuuan3620@gmail.com>
2024-08-10 02:59:15 +0700

    fix: declare tailwind module

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,25 +1,23 @@
 import React from 'react'
 
-declare module 'tailwindcss/lib/util/flattenColorPalette'
-
 type ReadonlyProps<T> = {
-    readonly [P in keyof T]: T[P]
+  readonly [P in keyof T]: T[P]
 }
 
 type PricingItemProps = {
-    title: string
-    subtitle: string
-    price: int
-    features: string[]
+  title: string
+  subtitle: string
+  price: int
+  features: string[]
 }
 
 type FeatureItemProps = {
-    title: string
-    icon: React.ReactElement
-    description: string
+  title: string
+  icon: React.ReactElement
+  description: string
 }
 
 declare global {
-    type FeatureItem = ReadonlyProps<FeatureItemProps>
-    type PricingItem = ReadonlyProps<PricingItemProps>
+  type PricingItem = ReadonlyProps<PricingItemProps>
+  type FeatureItem = ReadonlyProps<FeatureItemProps>
 }
